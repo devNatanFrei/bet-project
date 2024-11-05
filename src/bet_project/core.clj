4c8105d50269749febc6a98c0b47e4069d6884fc
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-11-05 14:41:26 -0300

    adicionei os oddsType

--- src/bet_project/core.clj
+++ src/bet_project/core.clj
@@ -3,13 +3,19 @@
             [cheshire.core :as json])
   (:gen-class))
 
-(def resp (client/get "https://betano.p.rapidapi.com/tournaments"
+(def tourn (client/get "https://betano.p.rapidapi.com/tournaments"
                       {:headers {"x-rapidapi-key" "7e74fa9991msh6adec433f460cb7p1f3802jsn94929cbbe680"
                                  "x-rapidapi-host" "betano.p.rapidapi.com"}
                        :query-params {"sport" "soccer"}}))
 
-(def body (:body resp))
-(def body-parsed (json/parse-string body true))
+(def oddsType (client/get "https://betano.p.rapidapi.com/oddstypes" {:headers {:x-rapidapi-key "7e74fa9991msh6adec433f460cb7p1f3802jsn94929cbbe680"
+                                                                 :x-rapidapi-host "betano.p.rapidapi.com"}
+                                                       :query-params {:sport "soccer"}}))              
+
+(def )
+
+(def body-tourn (:body tourn))
+(def body-parsed-tourn (json/parse-string body-tourn true))
 
 (defn -main
   [& args]
@@ -20,4 +26,6 @@
           (println "sportName:" (:sportName tournament))
           (println "tournamentId:" (:tournamentId tournament))
           (println "------------------"))
-        (vals body-parsed)))
+        (vals body-parsed-tourn))
+        
+)
