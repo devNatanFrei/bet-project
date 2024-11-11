0154f8022fa5b587a04ad76bf678839d13d7eff8
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-11-11 18:18:41 -0300

    adicionei os openers da uefaeuro e da nba

--- src/bet_project/core.clj
+++ src/bet_project/core.clj
@@ -1,116 +1,76 @@
 (ns bet-project.core
   (:require
-    [io.pedestal.http :as http]
-    [io.pedestal.http.route :as route]
-    [clj-http.client :as client]
-    [cheshire.core :as json]))
+   [io.pedestal.http :as http]
+   [io.pedestal.http.route :as route]
+   [cheshire.core :as json]
+   [clj-http.client :as client]))
 
-(def account (atom {}))
-(def bet (atom {}))
-
-(defn update-balance [amount]
-  (swap! account update :balance + amount))
+(def account-balance (atom 0))
 
 
-(defn deposit-value [request]
-  (-> request :json-params :value))
-
-
-
-(defn deposit [request]
-  (update-balance (deposit-value request))
-  {:status 200 
-   :body {:message (str "Depósito de " (deposit-value request) )
-          :balance @account}})
-  
-  
-
-(defn fetch-soccer-tournaments
-  []
-  (let [response (client/get "https://betano.p.rapidapi.com/tournaments" {:headers {:x-rapidapi-key "507110868dmsh80767a1dbb87630p1ce51fjsn9eaed59d8bcf"
-                                                                                    :x-rapidapi-host "betano.p.rapidapi.com"}
-                                                                          :query-params {:sport "soccer"}})]
-    (json/parse-string (:body response) true)))
-
-
-(defn fetch-events
-  []
-  (let [response(client/get "https://betano.p.rapidapi.com/events" {:headers {:x-rapidapi-key "507110868dmsh80767a1dbb87630p1ce51fjsn9eaed59d8bcf"
-                                                                              :x-rapidapi-host "betano.p.rapidapi.com"}
-                                                                    :query-params {:tournamentId "38"}})]
-    (json/parse-string (:body response) true)))
-
-(defn fetch-oddtypes
-  []
-  (let [response (client/get "https://betano.p.rapidapi.com/oddstypes" {:headers {:x-rapidapi-key "507110868dmsh80767a1dbb87630p1ce51fjsn9eaed59d8bcf"
-                                                                                  :x-rapidapi-host "betano.p.rapidapi.com"}
-                                                                        :query-params {:sport "soccer"}})]
-    (json/parse-string (:body response) true)))
-
-(defn parse-tournament
-  [tourn]
-  {:categoryName (:categoryName tourn)
-   :name         (:name tourn)
-   :sportName    (:sportName tourn)
-   :tournamentId (:tournamentId tourn)})
-
-(defn parse-event
-  [event]
-  {:bookmakerCount (:bookmakerCount event)
-   :date           (:date event)
-   :eventId        (:eventId event)
-   :eventStatus    (:eventStatus event)
-   :participant1   (:participant1 event)
-   :participant1Id (:participant1Id event)
-   :participant2   (:participant2 event)
-   :participant2Id (:participant2Id event)
-   :startTime      (:startTime event)
-   :time           (:time event)})
-
-(defn parse-oddtype
-  [odd]
-  {:handicap        (:handicap odd)
-   :marketId        (:marketId odd)
-   :marketName      (:marketName odd)
-   :marketNameShort (:marketNameShort odd)
-   :oddsType        (:oddsType odd)
-   :outcomeId       (:outcomeId odd)
-   :outcomeName     (:outcomeName odd)
-   :outcomes        (:outcomes odd)
-   :sport           (:sport odd)})
-
-(defn pegar-torneios-futebol
+(defn deposit-handler
   [request]
-  (let [tournaments (fetch-soccer-tournaments)]
+  (let [amount (:amount (json/parse-string (slurp (:body request)) true))]
+    (if (number? amount)
+      (do
+        (swap! account-balance + amount)
+        {:status 200})
+      )))
+
+
+(defn get-deposit [request]
+  {:status 200
+   :body (json/generate-string {:balance @account-balance})})
+
+
+(defn get-events-nba [request]
+  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/events/2022-12-20"
+                             {:headers {:x-rapidapi-key "8b7aaa01f5msh14e11a5a9881536p14b4b3jsn74e4cd56608c"
+                                        :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
+                              :query-params {:include "scores"
+                                             :affiliate_ids "1,2,3"
+                                             :offset "0"}})
+        data (:body response)]
     {:status 200
-     :body (mapv parse-tournament (vals tournaments))}))
+     :body data}))
 
-(defn pegar-eventos
-  [request]
-  (let [events (fetch-events)]
+
+(defn get-openers-nba [request]
+  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/openers/2024-11-11"
+                             {:headers {:x-rapidapi-key "8b7aaa01f5msh14e11a5a9881536p14b4b3jsn74e4cd56608c"
+                                        :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
+                              :query-params {:offset ""
+                                             :include "scores&include=all_periods"}})
+        data (:body response)]
     {:status 200
-     :body (mapv parse-event (vals (:events events)))}))
+     :body data}))
 
-(defn pegar-oddtypes
-  [request]
-  (let [oddtypes (fetch-oddtypes)]
+
+(defn get-openers-euro [request]
+  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/17/openers/2024-12-11"
+                             {:headers {:x-rapidapi-key "8b7aaa01f5msh14e11a5a9881536p14b4b3jsn74e4cd56608c"
+                                        :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
+                              :query-params {:include "scores&include=all_periods"}})
+        data (:body response)]
     {:status 200
-     :body (mapv parse-oddtype (vals oddtypes))}))
+     :body data}))
 
-(def routes (route/expand-routes
-              #{["/futebol" :get pegar-torneios-futebol :route-name :jogos-futebol] 
-                ["/eventos" :get pegar-eventos :route-name :todos-os-eventos]
-                ["/oddtypes" :get pegar-oddtypes :route-name :todos-os-oddtypes]}))
 
-(def service-map {::http/routes routes
-                  ::http/port   9999
-                  ::http/type   :jetty
-                  ::http/join?  false})
+(def routes
+  (route/expand-routes
+   #{["/deposit" :post deposit-handler :route-name :deposit]
+     ["/balance" :get get-deposit :route-name :get-balance]
+     ["/eventsnba" :get get-events-nba :route-name :get-events]
+     ["/openersnba" :get get-openers-nba :route-name :get-openers]
+     ["/openerseuro" :get get-openers-euro :route-name :get-openers-euro]}))
 
-(defn -main
-  [& args] 
+
+(def service-map
+  {::http/routes routes
+   ::http/port   9999
+   ::http/type   :jetty
+   ::http/join?  false})
+
+(defn -main [& args]
   (http/start (http/create-server service-map))
-  (println "Rodou pourra")
-  )
-
-
+  (println "Servidor iniciado!"))
