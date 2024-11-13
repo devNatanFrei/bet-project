232278a7a09bf8780060ef3dbd164568ce78f1fe
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-11-13 16:39:48 -0300

    adicionado a parte de apostas

--- src/bet_project/core.clj
+++ src/bet_project/core.clj
@@ -5,97 +5,97 @@
    [cheshire.core :as json]
    [clj-http.client :as client]))
 
-(def account-balance (atom 0))
+(def saldo-conta (atom 0))
+(def apostas (atom []))
 
-
-(defn deposit-handler
+(defn depositar-handler
   [request]
-  (let [amount (:amount (json/parse-string (slurp (:body request)) true))]
-    (if (number? amount)
+  (let [quantidade (:quantidade (json/parse-string (slurp (:body request)) true))]
+    (if (number? quantidade)
       (do
-        (swap! account-balance + amount)
-        {:status 200})
-      )))
+        (swap! saldo-conta + quantidade)
+        {:status 200
+         :body "Depósito realizado com sucesso!"})
+      {:status 400
+       :body "Valor inválido para depósito."})))
 
-
-(defn balance-handler
-  [request]
-  (let [amount (:amount (json/parse-string (slurp (:body request)) true))]
-    (if (number? amount)
-      (do
-        (swap! account-balance - amount)
-        {:status 200}))))
-
-(defn get-schedules-nba [request]
-  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/schedule" {:headers {:x-rapidapi-key "3e36075547msh24537dc0606651ap103e05jsna0572db9e77c"
-                                                                                                            :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
-                                                                                                  :query-params {:limit "100"}})
-                         data (:body response)]
-                     {:status 200
-                      :body data}))
-
-(defn get-schedules-euro [request]
-  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/17/schedule" {:headers {:x-rapidapi-key "3e36075547msh24537dc0606651ap103e05jsna0572db9e77c"
-                                                                                                            :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
-                                                                                                  :query-params {:limit "100"}})
-        data (:body response)]
-    {:status 200
-     :body data}))
-
-  (defn get-deposit [request]
+(defn obter-saldo-handler [request]
   {:status 200
-   :body (json/generate-string {:balance @account-balance})})
+   :body (json/generate-string {:saldo @saldo-conta})})
+
+(defn registrar-aposta-handler [request]
+  (let [aposta (json/parse-string (slurp (:body request)) true)
+        valor-aposta (:quantidade aposta)]
+    (if (and (number? valor-aposta) (<= valor-aposta @saldo-conta))
+      (do
+        (swap! saldo-conta - valor-aposta)
+        (swap! apostas conj aposta)
+        {:status 200 :body "Aposta registrada com sucesso"})
+      {:status 400 :body "Saldo insuficiente ou valor inválido para a aposta."})))
 
 
-(defn get-events-nba [request]
-  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/events/2022-12-20"
-                             {:headers {:x-rapidapi-key "8b7aaa01f5msh14e11a5a9881536p14b4b3jsn74e4cd56608c"
+(defn obter-aposta-handler [request]
+  {:status 200
+   :body (json/generate-string @apostas)})
+
+
+
+
+
+(defn obter-eventos-nba [request]
+  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/events/2024-11-13"
+                             {:headers {:x-rapidapi-key "sua-chave-api"
                                         :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
                               :query-params {:include "scores"
                                              :affiliate_ids "1,2,3"
                                              :offset "0"}})
-        data (:body response)]
-    {:status 200
-     :body data}))
+        dados (:body response)]
+    {:status 200 :body dados}))
 
-
-(defn get-openers-nba [request]
+(defn obter-mercados-nba [request]
   (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/openers/2024-11-11"
-                             {:headers {:x-rapidapi-key "8b7aaa01f5msh14e11a5a9881536p14b4b3jsn74e4cd56608c"
+                             {:headers {:x-rapidapi-key "sua-chave-api"
                                         :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
                               :query-params {:offset ""
                                              :include "scores&include=all_periods"}})
-        data (:body response)]
-    {:status 200
-     :body data}))
+        dados (:body response)]
+    {:status 200 :body dados}))
 
-
-(defn get-openers-euro [request]
-  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/17/openers/2024-12-11"
-                             {:headers {:x-rapidapi-key "8b7aaa01f5msh14e11a5a9881536p14b4b3jsn74e4cd56608c"
+(defn get-schedules-nba [request]
+  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/4/schedule"
+                             {:headers {:x-rapidapi-key "3e36075547msh24537dc0606651ap103e05jsna0572db9e77c"
                                         :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
-                              :query-params {:include "scores&include=all_periods"}})
+                              :query-params {:limit "100"}})
         data (:body response)]
     {:status 200
      :body data}))
 
+(defn get-schedules-euro [request]
+  (let [response (client/get "https://therundown-therundown-v1.p.rapidapi.com/sports/17/schedule"
+                             {:headers {:x-rapidapi-key "3e36075547msh24537dc0606651ap103e05jsna0572db9e77c"
+                                        :x-rapidapi-host "therundown-therundown-v1.p.rapidapi.com"}
+                              :query-params {:limit "100"}})
+        data (:body response)]
+    {:status 200
+     :body data}))
 
-(def routes
+(def rotas
   (route/expand-routes
-   #{["/deposit" :post deposit-handler :route-name :deposit]
-     ["/balance" :get get-deposit :route-name :get-balance]
-     ["/events-nba" :get get-events-nba :route-name :get-events]
-     ["/openers-nba" :get get-openers-nba :route-name :get-openers]
+   #{["/depositar" :post depositar-handler :route-name :depositar]
+     ["/saldo" :get obter-saldo-handler :route-name :saldo]
+     ["/apostar" :post registrar-aposta-handler :route-name :registrar-aposta]
+     ["/qtdApostada" :get obter-aposta-handler :route-name :obter-apostas]
+     ["/eventos-nba" :get obter-eventos-nba :route-name :eventos-nba]
+     ["/mercados-nba" :get obter-mercados-nba :route-name :mercados-nba]
      ["/schedules-nba" :get get-schedules-nba :route-name :get-nba-schedules]
-     ["/schedules-euro" :get get-schedules-euro :route-name :get-euro-schedules]
-     ["/openers-euro" :get get-openers-euro :route-name :get-openers-euro]}))
+     ["/schedules-euro" :get get-schedules-euro :route-name :get-euro-schedules]}))
 
-
-(def service-map
-  {::http/routes routes
+(def mapa-servico
+  {::http/routes rotas
    ::http/port   9999
    ::http/type   :jetty
    ::http/join?  false})
 
-  (http/start (http/create-server service-map))
-  (println "rodou porra")
+(defn -main []
+  (http/start (http/create-server mapa-servico))
+  (println "roda poooora"))
